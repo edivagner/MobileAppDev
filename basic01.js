66305ae4282b5d506f84d122edafa34f502b6994
edivagner@gmail.com <Bqsqddtasf2020?>
2022-10-07 20:28:21 +0100

    Basic variable, arithmetic and concatenation

---
+++ basic01.js
@@ -0,0 +1,13 @@
+//variable
+//number, string, boolean
+var firstName = "Edivagner"; // dinamica
+var firstName2 = "Edivagner"; // com a definicao do tipo da variavel
+var age = 28;
+age = 40;
+var flag = true;
+//arithmetic
+var x = 1;
+var y = 8;
+var result = x + y;
+console.log(x + " + " + y + " = " + result);
+//string concatenation
---
+++ basic01.ts
@@ -0,0 +1,23 @@
+//variable
+//number, string, boolean
+
+let firstName = "Edivagner"; // dinamica
+let firstName2: string = "Edivagner"; // com a definicao do tipo da variavel
+
+let age: number = 28;
+
+age = 40;
+
+let flag: boolean = true;
+
+
+//arithmetic
+
+let x: number = 1;
+let y = 8;
+
+let result = x + y;
+
+console.log(x + " + " + y + " = " + result)
+//string concatenation
+
