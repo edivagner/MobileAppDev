9db0b684ce404449c57bc8abf513bbda67e579e3
edivagner@gmail.com <Bqsqddtasf2020?>
2022-10-07 19:45:47 +0100

    hello teste

---
+++ hello.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Edivagner";
+console.log(greeter(user));
---
+++ hello.ts
@@ -0,0 +1,7 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+var user = "Edivagner";
+
+console.log(greeter(user));
